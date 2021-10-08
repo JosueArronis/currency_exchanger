bd9a0533cec1e1fb9b1c3a0d2b53e2a6c1947ba7
Josue Arronis <cruzjos94@gmail.com>
2021-10-08 07:10:51 -0600

    cache interface

---
+++ api/src/interfaces/cache_result_interface.ts
@@ -0,0 +1,5 @@
+export interface IcacheResult {
+  conversion_rate: number;
+  base_code: string;
+  target_code: string;
+}
--- api/src/middlewares/cache.ts
+++ api/src/middlewares/cache.ts
@@ -2,6 +2,7 @@ import NodeCache from 'node-cache';
 import { Request, Response, NextFunction } from 'express';
 import { ResponseInterface } from '../interfaces/response_interface';
 import { ApiError, handleError } from '../error';
+import { IcacheResult } from '../interfaces/cache_result_interface';
 
 export const cache = new NodeCache({ stdTTL: 15 }); // Just for example can configure it to get more time cache
 
@@ -9,17 +10,19 @@ export const verifyCache = (req: Request, res: Response, next: NextFunction) => 
   try {
     const { baseCurrency, quoteCurrency, baseAmount } = req.query;
     if (cache.has(`${baseCurrency}_${quoteCurrency}`)) {
-      const cache_result: any = cache.get(`${baseCurrency}_${quoteCurrency}`);
-      const response = {
-        exchangeRate: parseFloat(cache_result.conversion_rate.toFixed(3)),
-        quoteAmount: parseFloat(
-          (cache_result.conversion_rate * parseFloat(baseAmount as string) * 100).toFixed(3)
-        ),
-        base_code: cache_result.base_code,
-        target_code: cache_result.target_code,
-      } as ResponseInterface;
-
-      return res.status(200).json(response);
+      const cache_result: IcacheResult | undefined = cache.get(`${baseCurrency}_${quoteCurrency}`);
+      if (cache_result) {
+        const response = {
+          exchangeRate: parseFloat(cache_result.conversion_rate.toFixed(3)),
+          quoteAmount: parseFloat(
+            (cache_result.conversion_rate * parseFloat(baseAmount as string) * 100).toFixed(3)
+          ),
+          base_code: cache_result.base_code,
+          target_code: cache_result.target_code,
+        } as ResponseInterface;
+        return res.status(200).json(response);
+      }
+      return next();
     }
     return next();
   } catch (error) {
